7c068e924029452d547945ba2987bd82bcb1b17a
Jesper Frickmann <jesper.frickmann@hotmail.com>
2018-07-16 20:54:51 -0400

    Show GVs on QX7

--- SCRIPTS/TELEMETRY/JF3Kadjmix.lua
+++ SCRIPTS/TELEMETRY/JF3Kadjmix.lua
@@ -1,5 +1,5 @@
 -- JF F3K mix adjustment
--- Timestamp: 2018-07-15
+-- Timestamp: 2018-07-16
 -- Created by Jesper Frickmann
 
 local gv3 = getFieldInfo("gvar3").id
@@ -22,23 +22,23 @@ if tx == TX_X9D then
 		
 		-- Draw instructions on the screem
 		DrawMenu(" Adjust mixes ")
+
+		lcd.drawText(10, 14, "Rudder trim = Aile-rudder", 0)
+		lcd.drawText(10, 26, "Aileron trim = Differential", 0)
+		lcd.drawText(10, 38, "Elevator trim = Brake-elev.", 0)
+		lcd.drawText(10, 50, "Throttle trim = Snap-flap", 0)
+		
+		lcd.drawLine(155, 10, 155, 61, SOLID, FORCE)
 		
 		lcd.drawText(160, 14, "AiR")
 		lcd.drawText(160, 26, "Dif")
 		lcd.drawText(160, 38, "BkE")
 		lcd.drawText(160, 50, "Snp")
 
-		lcd.drawLine(155, 10, 155, 61, SOLID, FORCE)
-		
 		lcd.drawNumber(202, 14, getValue(gv3), RIGHT)
 		lcd.drawNumber(202, 26, getValue(gv4), RIGHT)
 		lcd.drawNumber(202, 38, getValue(gv5), RIGHT)
 		lcd.drawNumber(202, 50, getValue(gv6), RIGHT)
-
-		lcd.drawText(10, 14, "Rudder trim = Aile-rudder", 0)
-		lcd.drawText(10, 26, "Aileron trim = Differential", 0)
-		lcd.drawText(10, 38, "Elevator trim = Brake-elev.", 0)
-		lcd.drawText(10, 50, "Throttle trim = Snap-flap", 0)
 	end -- run()
 else
 	function run(event)
@@ -53,22 +53,22 @@ else
 		-- Draw instructions on the screem
 		DrawMenu("Adjust mixes")
 
+		lcd.drawText(5, 14, "Rudder trim =", SMLSIZE)
+		lcd.drawText(5, 26, "Aileron trim =", SMLSIZE)
+		lcd.drawText(5, 38, "Elevator trim =", SMLSIZE)
+		lcd.drawText(5, 50, "Throttle trim =", SMLSIZE)
+
+		lcd.drawLine(75, 10, 75, 61, SOLID, FORCE)
+		
 		lcd.drawText(85, 14, "AiR", SMLSIZE)
 		lcd.drawText(85, 26, "Dif", SMLSIZE)
 		lcd.drawText(85, 38, "BkE", SMLSIZE)
 		lcd.drawText(85, 50, "Snp", SMLSIZE)
 
-		lcd.drawLine(75, 10, 75, 61, SOLID, FORCE)
-		
 		lcd.drawNumber(123, 14, getValue(gv3), RIGHT + SMLSIZE)
 		lcd.drawNumber(123, 26, getValue(gv4), RIGHT + SMLSIZE)
 		lcd.drawNumber(123, 38, getValue(gv5), RIGHT + SMLSIZE)
 		lcd.drawNumber(123, 50, getValue(gv6), RIGHT + SMLSIZE)
-
-		lcd.drawText(5, 14, "Rudder trim =", SMLSIZE)
-		lcd.drawText(5, 26, "Aileron trim =", SMLSIZE)
-		lcd.drawText(5, 38, "Elevator trim =", SMLSIZE)
-		lcd.drawText(5, 50, "Throttle trim =", SMLSIZE)
 	end -- run()
 end
 
--- SCRIPTS/TELEMETRY/JFXJailcmb.lua
+++ SCRIPTS/TELEMETRY/JFXJailcmb.lua
@@ -1,5 +1,5 @@
 -- JF FXJ aileron and camber adjustment
--- Timestamp: 2018-05-20
+-- Timestamp: 2018-07-16
 -- Created by Jesper Frickmann
 
 local gv1 = getFieldInfo("gvar1").id
@@ -13,27 +13,37 @@ if tx == TX_X9D then
 	function Draw()
 		DrawMenu(" Aileron and camber ")
 		
+		lcd.drawText(10, 18, "Aileron trim = aileron", 0)
+		lcd.drawText(10, 30, "Rudder trim = flaperon", 0)
+		lcd.drawText(10, 42, "Elev. trim = aileron camber", 0)
+
+		lcd.drawLine(155, 10, 155, 61, SOLID, FORCE)
+		
 		lcd.drawText(160, 18, "Ail")
 		lcd.drawText(160, 30, "AiF")
 		lcd.drawText(160, 42, "CbA")
 
-		lcd.drawLine(155, 10, 155, 61, SOLID, FORCE)
-		
 		lcd.drawNumber(202, 18, getValue(gv1), RIGHT)
 		lcd.drawNumber(202, 30, getValue(gv2), RIGHT)
 		lcd.drawNumber(202, 42, getValue(gv5), RIGHT)
-
-		lcd.drawText(10, 18, "Aileron trim = aileron", 0)
-		lcd.drawText(10, 30, "Rudder trim = flaperon", 0)
-		lcd.drawText(10, 42, "Elev. trim = aileron camber", 0)
 	end -- Draw()
 else
 	function Draw()
-		DrawMenu("Aileron & camber")
+		DrawMenu("Aile & camber")
 
-		lcd.drawText(5, 18, "Aileron trim = aileron", SMLSIZE)
-		lcd.drawText(5, 30, "Rudder trim = flaperon", SMLSIZE)
-		lcd.drawText(5, 42, "Elev. trim = aileron camber", SMLSIZE)
+		lcd.drawText(5, 18, "Aileron trim =", SMLSIZE)
+		lcd.drawText(5, 30, "Rudder trim =", SMLSIZE)
+		lcd.drawText(5, 42, "Elevator trim =", SMLSIZE)
+
+		lcd.drawLine(75, 10, 75, 61, SOLID, FORCE)
+		
+		lcd.drawText(85, 18, "Ail", SMLSIZE)
+		lcd.drawText(85, 30, "AiF", SMLSIZE)
+		lcd.drawText(85, 42, "CbA", SMLSIZE)
+
+		lcd.drawNumber(123, 18, getValue(gv1), RIGHT + SMLSIZE)
+		lcd.drawNumber(123, 30, getValue(gv2), RIGHT + SMLSIZE)
+		lcd.drawNumber(123, 42, getValue(gv5), RIGHT + SMLSIZE)
 	end -- Draw()
 end
 
