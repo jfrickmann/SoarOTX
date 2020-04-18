40ac386f7656d78398a4a4c646f6aaddc0844649
Jesper Frickmann <jesper.frickmann@hotmail.com>
2020-04-18 16:08:44 -0400

    More contrast

--- SD-CARD/SCRIPTS/TELEMETRY/212x64/ARMED.lua
+++ SD-CARD/SCRIPTS/TELEMETRY/212x64/ARMED.lua
@@ -1,9 +1,10 @@
 -- 212x64/ARMED.lua
--- Timestamp: 2020-04-13
+-- Timestamp: 2020-04-18
 -- Created by Jesper Frickmann
 
 local function FlashArmed()
-	lcd.drawText(50, 28, "MOTOR  ARMED", DBLSIZE + BLINK + INVERS)
+	lcd.drawFilledRectangle(30, 20, 152, 32, FORCE)
+	lcd.drawText(44, 28, "MOTOR  ARMED", DBLSIZE + BLINK + INVERS)
 end
 
 return FlashArmed
